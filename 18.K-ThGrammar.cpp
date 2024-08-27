661790944f1736e1c53ff7129c90d70e92f72cca
CoderMayukh12 <mayukhbhowmik.cse2022@nsec.ac.in>
2024-08-27 21:07:31 +0530

    Leetcode KTh Grammar added!

---
+++ 18.K-ThGrammar.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int kthGrammar(int n, int k)
+{
+    if (n == 1 && k == 1)
+        return 0;
+    int row = (1 << (n - 1));
+    int mid = row / 2;
+    if (k <= mid)
+    {
+        return kthGrammar(n - 1, k);
+    }
+    else
+    {
+        return !kthGrammar(n - 1, k - mid);
+    }
+}
